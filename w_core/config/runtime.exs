83be2f71b3f05fef70bea20eee02e1883564365e
skempfer <shana.kempfer88@gmail.com>
2026-04-03 13:56:47 -0300

    chore: initialize Phoenix 1.7 project with --live and SQLite3

---
+++ w_core/config/runtime.exs
@@ -0,0 +1,44 @@
+import Config
+
+if System.get_env("PHX_SERVER") do
+  config :w_core, WCoreWeb.Endpoint, server: true
+end
+
+config :w_core, WCoreWeb.Endpoint, http: [port: String.to_integer(System.get_env("PORT", "4000"))]
+
+if config_env() == :prod do
+  database_path =
+    System.get_env("DATABASE_PATH") ||
+      raise """
+      environment variable DATABASE_PATH is missing.
+      For example: /etc/w_core/w_core.db
+      """
+
+  config :w_core, WCore.Repo,
+    database: database_path,
+    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "5")
+
+  secret_key_base =
+    System.get_env("SECRET_KEY_BASE") ||
+      raise """
+      environment variable SECRET_KEY_BASE is missing.
+      You can generate one by calling: mix phx.gen.secret
+      """
+
+  host = System.get_env("PHX_HOST") || "example.com"
+
+  config :w_core, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
+
+  config :w_core, WCoreWeb.Endpoint,
+    url: [host: host, port: 443, scheme: "https"],
+    http: [
+      ip: {0, 0, 0, 0, 0, 0, 0, 0}
+    ],
+    secret_key_base: secret_key_base
+
+database_path = System.get_env("DATABASE_PATH") || "w_core_prod.sqlite3"
+config :w_core, WCore.Repo,
+  database: database_path,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+
+end
